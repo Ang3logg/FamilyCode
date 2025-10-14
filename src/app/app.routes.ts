0f7b8c6cd4b4c69d64bfd74883a002565fc3263a
Alphonse <vyma74@gmail.com>
2025-10-13 20:00:30 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Servicios } from './pages/servicios/servicios';
+import { Portafolio } from './pages/portafolio/portafolio';     
+import { Contacto } from './pages/contacto/contacto';
+import { Blog } from './pages/blog/blog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: Home},
+    {path: 'servicios',component: Servicios},
+    {path: 'portafolio',component: Portafolio},
+    {path: 'contacto',component: Contacto},
+    {path: 'blog',component: Blog},
+    {path: '**',redirectTo: ''} //cualquier otra ruta redirige a home
+
+];
